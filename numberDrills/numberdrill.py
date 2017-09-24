bd0516be9d50fa1609a19f716b07c054b86591de
James Lownie <jlownie@hotmail.com>
2017-09-24 12:20:28 +1000

    About to add playback for wavs. Have done some of the changes already

--- numberDrills/numberdrill.py
+++ numberDrills/numberdrill.py
@@ -1,23 +1,28 @@
-import random, sys, tty, termios
-
-# Initialise terminal so we can catch characters from input
-fd = sys.stdin.fileno()
-old_settings = termios.tcgetattr(fd)
-
-japaneseNumbers = ["maru", "ichi", "ni", "san", "shi", "go", "roku", "shichi", "hachi", "kyuu", "juu"]
-
-random.seed()
+import random, sys, tty, termios, pygame.mixer
 
 class question:
 	number = 0
 	text = ""
 	waveFile = ""
 
+def prepareQuestions():
+	counter = 0
+	questionArray = []
+	
+	for numberText in ["maru", "ichi", "ni", "san", "shi", "go", "roku", "shichi", "hachi", "kyuu"]:
+		newQuestion = question()
+		newQuestion.number = counter
+		newQuestion.text = numberText
+		questionArray.append(newQuestion)
+		counter = counter + 1
+	
+	print "Number of questions is " + str(len(questionArray))
+	return questionArray
+
 def getQuestion():
-	newQuestion = question()
-	newQuestion.number = random.randint(0,9)
-	newQuestion.text = japaneseNumbers[newQuestion.number]
-	return newQuestion
+	questionChoice = random.randint(0, len(japaneseNumbers) - 1)
+	thisQuestion = japaneseNumbers[questionChoice]
+	return thisQuestion
 
 def getKeyPress():
 	# Get input from the user
@@ -26,7 +31,20 @@ def getKeyPress():
 		answer = sys.stdin.read(1)
 	finally:
 		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+	return answer
 
+# Code execution starts here
+
+# Initialise terminal so we can catch characters from input
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
+
+random.seed()
+pygame.mixer.init()
+
+japaneseNumbers = prepareQuestions()
+
+# Ask questions until the user chooses to exit
 while True:
 	thisQuestion = getQuestion()
 	print thisQuestion.text
